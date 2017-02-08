a52c697cb26720fe29cf74f8644ea55eb4544ae4
Mudit Garodia <mgarodia@ucsd.edu>
2017-02-08 15:36:42 -0800

    update

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/flow', flow.view);
-// app.get('/poses', poses.view);
+app.get('/poses', poses.view);
 app.get('/play', play.view);
 
 // Example route
