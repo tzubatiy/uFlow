b010e7dde93679d353dca8fcf0bf43649f764f4b
Tamara Zubatiy <tzubatiy@ucsd.edu>
2017-02-08 10:41:46 -0800

    fix it

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+
+var flow = require('./routes/flow');
 // Example route
 // var user = require('./routes/user');
 
@@ -36,6 +38,8 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
+app.get('/flow', flow.view);
+
 // Example route
 // app.get('/users', user.list);
 
