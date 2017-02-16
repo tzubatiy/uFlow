02e11040a05bf1ab10952a606b18a5c04c893c1f
Mudit Garodia <mgarodia@ucsd.edu>
2017-02-16 14:12:44 -0800

    francis code added

--- routes/poses.js
+++ routes/poses.js
@@ -1,9 +1,10 @@
-
 /*
  * GET poses page.
  */
 var data = require('../data.json');
 
 exports.view = function(req, res){
-  res.render('poses');
+  res.render('poses', {
+  	'poses' :data
+  });
 };
