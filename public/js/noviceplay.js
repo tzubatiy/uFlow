206495e7d53364af51819ed857f3c2e6b3934581
Mudit Garodia <mgarodia@ucsd.edu>
2017-02-16 19:06:25 -0800

    added audio code

---
+++ public/js/noviceplay.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	console.log("Playing file")
+	$("#play").click(function(e) {
+		var audio = new Audio('nov20.mp3');
+		audio.play();
+	});
+
+	// Add any additional listeners here
+	// example: $("#div-id").click(functionToCall);
+}
