d5fc177c525fba6710138fe70d932566b40b2056
Mudit Garodia <mgarodia@ucsd.edu>
2017-02-16 18:43:55 -0800

    poses page functional

--- routes/poses.js
+++ routes/poses.js
@@ -7,4 +7,36 @@ exports.view = function(req, res){
   res.render('poses', {
   	'poses' :data
   });
+
+
+/*function initializePage() {
+	$('.project a').click(addProjectDetails);
+
+	$('#colorBtn').click(randomizeColors);
+}
+
+ function addProjectDetails(e) {
+	// Prevent following the link
+	e.preventDefault();
+
+	// Get the div ID, e.g., "project3"
+	var projectID = $(this).closest('.project').attr('id');
+	// get rid of 'project' from the front of the id 'project3'
+	var idNumber = projectID.substr('project'.length);
+
+	console.log("User clicked on project " + idNumber);
+
+	$.get('/project/'+ idNumber, addProject);
+}
+
+function addProject(result){
+	console.log(result);
+	var projectHTML= '<a href="#" class= "thumbnail">' +
+		'<img src='+ result['image'] + '"class= "img">' +
+		'<p>' + result['title']+ '</p>' + 
+		'<p><small>' + result['summary']+ '</small></p></a>';
+
+	$('#'+'project'+result.id+ ' .details').html(result['summary'])
+
+}*/
 };
