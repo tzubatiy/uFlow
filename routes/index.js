4eca37af25f3b118ddf392b563a7ea3cd4d8be12
Francis Anyim <fanyim@ucsd.edu>
2017-02-21 14:33:20 -0800

    starting to add the push

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,13 @@
 /*
  * GET home page.
  */
+ /*
+ * Push data into flows done.
+ */
+var data = require('../profiles.json');
 
 exports.view = function(req, res){
-  res.render('index');
+  res.render('index' ;{
+  	'index' :data
+  })
 };
