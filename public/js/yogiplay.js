46a03f5769046b40ea1ccfdfe28d647a2d250717
Francis Anyim <fanyim@ucsd.edu>
2017-02-26 18:49:11 -0800

    fixed pause/play for yogi, took off nav bar below it

--- public/js/yogiplay.js
+++ public/js/yogiplay.js
@@ -9,20 +9,45 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+	var isPlaying = false;
+	var isPaused = false;
+	var audio = new Audio('int20.mp3');
 	console.log("Playing file")
+	$("#pause").hide();
 	$("#play").click(function(e) {
 		var time = $("#time option:selected").text();
 		console.log("profile/" + time);
 
 		$.get("/profile/" + time, addFlowsdone);
-		var audio = new Audio('int20.mp3');
 		audio.play();
-	});
+		isPlaying = true;
+		$("#play").hide();
+		$("#pause").show();
+	})	
+
+	if(isPlaying = true){
+		$("#pause").click(function(e) {
+			audio.pause();
+			isPlaying = false;
+			isPaused = true;
+			$("#play").show();
+			$("#pause").hide();
+		})
+	}	else if(isPaused = true){
+		$("play").click(function(e) {
+			audio.play();
+			isPlaying = true;
+			isPaused = false;
+			$("#play").hide();
+			$("#pause").show();
+		})
+	}	
 
 function addFlowsdone(e){
 	console.log("Flows will be added");
 
 }
+
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
 }
